1404df32bf1e0690e1843472b43748a7b3c9e055
Josue <144635031+JosueTije@users.noreply.github.com>
2024-11-06 15:30:09 -0600

    Update URLS y comentarios

--- src/constants.js
+++ src/constants.js
@@ -19,22 +19,32 @@ const serverPort = 3000
 const contextURL = '/iot'; //If needed, project context
 const api = '/api'; // Sugested API URL
 
-//SENSOR 1 URLS. Configurar URLS por cada sensor.
+//URLS para los sensores
+
+//SENSOR 1. *SENSOR DE TEMPERATURA*
 const getTemperatureSensor = '/getTemperatures'
 const getTemperatureSensorByDate = '/getTemperatures'
 const postTemperatureSensor = '/insertTemperature' //Implemented Endpoint URL
+
+//SENSOR 2. *SENSOR DE COLOR*
 const getColorSensor = '/getColors'
 const getColorSensorByDate = '/getColors'
 const postColorSensor = '/insertColor';
 
+//SENSOR 3. *SENSOR DE PROXIMIDAD*
+const getDistanceSensor = '/getDistance'
+const getDistanceSensorByDate = '/getDistance'
+const postDistanceSensor = '/insertDistance';
 
-/*
- * DB Queries
- * Agregar queries por sensor.
- */
+
+//Queries Base de Datos
+
+//Sensor de temperatura
 const selectTemperature = 'SELECT * FROM temps';
 const selectTemperatureByDate = 'SELECT * FROM temps WHERE fecha between ? and ?';
 const insertTemperature = 'INSERT INTO temps (valor) values (?)';
+
+//Sensor de color
 const selectColorSensor = 'SELECT * FROM color_sensor';
 const selectColorSensorByDate = 'SELECT * FROM color_sensor WHERE fecha between ? and ?';
 const insertColorSensor = 'INSERT INTO color_sensor (val, color) values (?, ?)';
