2f20792bf7bec26827bd5747036395cbe1ffed9a
Josue <josue.tijerina@outlook.com>
2024-11-08 13:15:38 -0600

    actualización codigo de arduino

--- src/nodemcu/nodemcu.ino
+++ src/nodemcu/nodemcu.ino
@@ -4,8 +4,8 @@
 #include <DHT.h> // https://github.com/adafruit/DHT-sensor-library
 
 // Sensor de distancia Ultrasónico HC-SR04.
-#define Trig 13 // "Trig" 
-#define Echo 15 // "Echo"
+#define Trig D5 // "Trig" 
+#define Echo D6 // "Echo"
 
 #define WIFI_SSID "Tec-IoT"
 #define WIFI_PASSWORD "spotless.magnetic.bridge"
@@ -24,7 +24,6 @@ String deviceID1 = "1/";
 int lecturaSensorA0 = 0; // Lectura sensor analogico
 int umbral_A0 = 500; // Criterio para enviar un mensaje al "broker", Sensor analogico
 int umbral_D = 40;
-float voltage = 0;
 
 void setup() {
 pinMode( Trig, OUTPUT); // Configuracion
@@ -33,10 +32,11 @@ digitalWrite(Trig, LOW); // Inicializar
 
 // Indicadores
 Serial.begin(9600); // Puerto
-dht.begin();
+
 WiFi.mode(WIFI_STA);
 WiFi.disconnect();
 delay(100);
+
 WiFi.begin(ssid, password);
 Serial.print("Conectando a red WiFi \"");
 Serial.print(ssid);
@@ -48,61 +48,21 @@ Serial.print(".");
 Serial.print("\nConectado! IP: ");
 Serial.println(WiFi.localIP());
 delay(500);
+
 }
+
+
 void loop() {
 long t, d;
 float h;
 float tmp;
-String msg_str = "Hello World";
-int lecturaSensorA0 = analogRead(A0); // **** Sensor analogico
-float voltage = lecturaSensorA0 * (3.3 / 1023.0); //escalamos a voltaje msg_str = "The resistance value is: ";
+long distancia = distanciaUltrasonico(Trig, Echo);
+Serial.print("Distancia:");
+Serial.print(distanciaIzquierdo);
+Serial.println(" cm");
+delay(1000);
+}
 
-//Enviamos por el puerto serie
-if(voltage <= 1){
-delay(2000);
-Serial.print(" LED ON");
-digitalWrite(Lamp1, HIGH);
-}
-else{
-delay(2000);
-Serial.print(" LED OFF");
-digitalWrite(Lamp1, LOW);
-}
-Serial.print(" ADC= ");
-Serial.print(lecturaSensorA0);
-Serial.print(" Voltaje= ");
-Serial.println(voltage);
-delay(2000);
-// Esperar 3 segundos
-h = dht.readHumidity(); // Leer la humedad relativa
-tmp = dht.readTemperature(); // Leer teperatura en grados centígrados
-(dht.readTemperature(true) en grados Fahrenheit)
-// Comprobamos si ha habido algún error en la lectura
-if (isnan(h) || isnan(tmp) ) {
-Serial.println("Error obteniendo los datos del sensor DHT11");
-//return;
-}
-msg_str = "The Temperature value is: "; // Construye mensaje (manejado como STRING).
-Serial.println(msg_str);
-Serial.println(tmp);//msg_str= msg_str + tmp;
-//msg_str.toCharArray(msg_chr, 58); // Copies the msg’s characters to the supplied
-buffer(message): STRING -> Char Array.
-// Muestra mensaje localmente.
-if ( isnan(tmp) == 0){
-logIntentoGETselect(deviceID1);
-// delay(2000);
-// Usamos metodo POST para insertar en la base de datos
-logIntentoPOSinsert(deviceID1, tmp);
-}
-delay(30000);
-msg_str = "The humidity value is: "; // Construye mensaje (manejado como STRING).
-Serial.println(msg_str);
-Serial.println(h);
-//msg_str= msg_str + h;
-// msg_str.toCharArray(msg_chr, 58); // Copies the msg’s characters to the supplied
-buffer(message): STRING -> Char Array.
-// Muestra mensaje localmente.
-}
 // Metodo GET para consultar la base de datos
 void logIntentoGETselect(String deviceID){
 //if(WiFi.status() == WL_CONNECTED){
@@ -145,6 +105,18 @@ Serial.println(httpClient.getString());
 httpClient.end();
 }
 return;
+}
+
+long distanciaUltrasonico(int Trig, int Echo) {
+    digitalWrite(Trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trig, LOW);
+
+    long duracion = pulseIn(echoPin, HIGH);
+    long distancia = duracion * 0.034 / 2;
+    return distancia;
 }
 
 
