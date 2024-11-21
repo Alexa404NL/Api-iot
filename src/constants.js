980bbaea826f7704abe78cafedb71e72b026b376
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2024-11-20 18:25:23 -0600

    cambio a constants de insert de valores

--- src/constants.js
+++ src/constants.js
@@ -44,7 +44,7 @@ const insertTemperature = 'INSERT INTO temps (valor) values (?)';
 //Sensor de color
 const selectColorSensor = 'SELECT * FROM color';
 const selectColorSensorByDate = 'SELECT * FROM color WHERE fecha between ? and ?';
-const insertColorSensor = 'INSERT INTO color (id, R, G, B, Color, fecha) VALUES (?, ?, ?, ?, ?, ?)';
+const insertColorSensor = 'INSERT INTO color (r, g, b, color) VALUES (?, ?, ?, ?)';
 
 // Sensor de distancia
 const selectDistance = 'SELECT * FROM distancia';
